140b32117311aaa2ae43934d3aa3c5cb8971d263
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-08-31 15:39:32 -0500

    completed exercise5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,25 @@
 const sumAll = function(start, end) {
 
+  // if non-number parameter is encountered.
+  if(start<0 || end<0 ||
+    typeof start !== "number" ||
+    typeof end !== "number"){
+   return "ERROR";
+  }
+
   let sum = 0;
-  for(let i=start; i<=end; i++){
-    sum += i;
+  // if smaller number is specified first.
+  if(start <= end){
+    for(let i=start; i<=end; i++){
+      sum += i;
+    }
+  }
+
+  // if bigger number is specified first.
+  else{
+    for(let i=start; i>=end; i--){
+      sum += i;
+    }
   }
   return sum;
 };
