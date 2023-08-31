e414184f80d1cbe5921a70571b42ea4ac1fe7b97
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-08-31 15:44:43 -0500

    fixed sumAll a little bit more optimally by just having 1 loop, instead of 2 conditions with 2 loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -7,20 +7,18 @@ const sumAll = function(start, end) {
    return "ERROR";
   }
 
-  let sum = 0;
-  // if smaller number is specified first.
-  if(start <= end){
-    for(let i=start; i<=end; i++){
-      sum += i;
-    }
+  // if bigger number is specified first.
+  if(start > end){
+    let temp = start;
+    start = end;
+    end = temp;
   }
 
-  // if bigger number is specified first.
-  else{
-    for(let i=start; i>=end; i--){
-      sum += i;
-    }
+  let sum = 0;
+  for(let i=start; i<=end; i++){
+    sum += i;
   }
+
   return sum;
 };
 
