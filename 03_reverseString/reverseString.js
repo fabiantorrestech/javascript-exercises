d26121a7c71cb2e0955fd4efe87c8c7983c0dc3a
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-08-31 13:39:51 -0500

    completed test 3 with 3 ways of reversing a string in JS .exit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(inputStr) {
+  // there is no easy method to reverse a string in JS...
+
+  // method 1 -- use a decrementing for loop and a new result string.
+  let result = "";
+  for(let i=inputStr.length-1; i >= 0; i--){
+    result += inputStr[i];
+  }
+  return result;
+
+  // method 2 -- use split(), reverse(), join("").
+  // - split("") will convert the string to an array
+  // - reverse() will reverse the array
+  // - join("") will join the array into a string, without the commas.
+  // return inputStr.split("").reverse().join("");
 
 };
 
