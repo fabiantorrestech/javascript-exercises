9244c49a99ac3ddd2e408f2bef0994a748fd6b02
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-08-31 15:28:12 -0500

    devised optimal-enough solution for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(inputArr, ...args) {
+
+  // method 1 -- naive solution i created
+  // - first loop through all extra arguments
+  // - compare each argument to each element in the array, 1 at a time (essentially a double for-loop)
+
+  // let resultArray = inputArr;
+  // for(let i=0; i<args.length; i++){
+  //   resultArray = resultArray.filter((num) => {
+  //     return num !== args[i];
+  //   });
+  // }
+  // return resultArray;
+
+  // method 2 -- a cleaner approach to my 1st solution.
+  // - tells the function to look through each element of inputArr with filter()
+  // - in the filter function, we say to only inlcude inputArr elements NOT FOUND in the extra arguments.
+
+  return inputArr.filter((element) => {
+    return !args.includes(element);
+  });
 
 };
 
