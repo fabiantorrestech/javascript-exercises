2b2b4b3de6c61ba7e86a0cf6b4aa803dedef1feb
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-08-31 23:25:07 -0500

    finished new problems

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farTemp) {
+  let result = Math.round(((farTemp-32)*(5/9)) * 1e1)/1e1;
+
+  // if result is a clean number, return a clean number with no decimal.
+  if(result%1 === 0){
+    result = Math.round(result);
+  }
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  let result = Math.round(((celTemp*(9/5))+32) * 1e1)/1e1;
+
+  // if result is a clean number, return a clean number with no decimal.
+  if(result%1 === 0){
+    result = Math.round(result);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
