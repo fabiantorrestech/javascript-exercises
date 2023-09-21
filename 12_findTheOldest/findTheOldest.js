2c404480a095f45695276bd2d09788df0f4ed253
Fabian Torres <46858378+fabiantorrestech@users.noreply.github.com>
2023-09-21 18:08:07 -0500

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+  // sort from oldest to youngest
+  let oldestToYoungest = people.sort((a, b) => {
+    let mostRecentYearA = a.yearOfDeath ? a.yearOfDeath : (new Date().getFullYear());
+    let mostRecentYearB = b.yearOfDeath ? b.yearOfDeath : (new Date().getFullYear());
+
+    let ageA = mostRecentYearA - a.yearOfBirth;
+    let ageB = mostRecentYearB - b.yearOfBirth;
+    return ageA > ageB ? -1 : 1;
+  });
+
+  // return the 1st entry.
+  return oldestToYoungest[0];
 };
 
 // Do not edit below this line
